a2331b55468ac524f0c036f16468de76fee88988
Phan Quang <phanlequang99@gmail.com>
2020-01-15 21:36:03 +0700

    Fix issue clearInterval with a wrong variable

--- index.js
+++ index.js
@@ -8,32 +8,32 @@ class Heroku {
     this.interval = undefined;
   }
   start() {
-      if (!this.appName) {
-        console.error(
-          "Your app name is invalid. Please declare it in constructor of awake-heroku"
-        );
-        return false;
-      }
-      if (this.time >= MAX_TIME) {
-        console.error("Your period time must <1 800 000 millisecond");
-      }
-      console.log("\x1b[36m%s\x1b[0m", `Your app name : ${this.appName}`);
-
-      console.log(
-        "\x1b[36m%s\x1b[0m",
-        `Period time want wakeup : ${this.time} millisecond`
+    if (!this.appName) {
+      console.error(
+        "Your app name is invalid. Please declare it in constructor of awake-heroku"
       );
-      this.interval = setInterval(async () => {
-        try {
-          await axios.get(`https://${this.appName}.herokuapp.com`);
-          console.log(
-            "\x1b[33m%s\x1b[0m",
-            "[Awake-Heroku] : Your app has been get up"
-          );
-        } catch (e) {
-          console.error("Your app name is invalid");
-        }
-      }, this.time);
+      return false;
+    }
+    if (this.time >= MAX_TIME) {
+      console.error("Your period time must <1 800 000 millisecond");
+    }
+    console.log("\x1b[36m%s\x1b[0m", `Your app name : ${this.appName}`);
+
+    console.log(
+      "\x1b[36m%s\x1b[0m",
+      `Period time want wakeup : ${this.time} millisecond`
+    );
+    this.interval = setInterval(async () => {
+      try {
+        await axios.get(`https://${this.appName}.herokuapp.com`);
+        console.log(
+          "\x1b[33m%s\x1b[0m",
+          "[Awake-Heroku] : Your app has been get up"
+        );
+      } catch (e) {
+        console.error("Your app name is invalid");
+      }
+    }, this.time);
   }
 
   setAppName(name) {
@@ -44,7 +44,7 @@ class Heroku {
   }
 
   stop() {
-    clearInterval(this.time);
+    clearInterval(this.interval);
   }
 }
 
